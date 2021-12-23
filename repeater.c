0295045ee73ba0258267fb6535122f7ba03ec347
sufjgourh57 <90439879+sufjgourh57@users.noreply.github.com>
2021-12-24 07:10:34 +0800

    Create repeater.c

---
+++ repeater.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int input;
+
+    printf("�C��J�@�ӼƦr�A�{���|�A�L�X�ۦP���Ʀr�C\n");
+    //fill some condition into the following while loop
+    //only ONE line code is you need
+    //please refer page 4 of homework request
+    while (scanf("%d", &input)) {
+        printf("%d\n", input);
+    }
+}
